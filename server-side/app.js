1cac487b1fbad5b1b9a482d77ac5fcc0ab54d58a
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-11-27 00:45:44 +0600

    ...

--- server-side/app.js
+++ server-side/app.js
@@ -9,8 +9,6 @@ const hpp=require('hpp');
 const cors=require('cors');
 const mongoose=require('mongoose');
 
-
-
 app.use(cors())
 app.use(helmet())
 app.use(mongoSanitize())
@@ -19,10 +17,8 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit:'50mb'}));
 app.use(bodyParser.json())
 
-
 const limiter=rateLimit({windowMs:15*60*1000,max:300});
 app.use(limiter);
-
 
 let URI="mongodb+srv://Sinister:XIII1233699877415XIII@cluster0.9lwqvdx.mongodb.net/CRUD1";
 let OPTION={user:'Sinister',pass:'XIII1233699877415XIII',autoIndex:true};
@@ -31,8 +27,6 @@ mongoose.connect(URI,OPTION).then((res)=>{
 }).catch((err)=>{
     console.log(err)
 })
-
-
 
 app.use("/api/v1",router);
 app.use("*",(req,res)=>{
